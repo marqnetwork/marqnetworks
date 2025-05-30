4fb440f3af6d7f02869d87bea354b370e762b8fc
muhammadsaad123-coder <130562696+muhammadsaad123-coder@users.noreply.github.com>
2025-05-30 20:46:17 +0500

    slider

--- src/components/ImageSlider/ImageSlider.jsx
+++ src/components/ImageSlider/ImageSlider.jsx
@@ -5,13 +5,21 @@ import { motion } from "framer-motion";
 import "./ImageSlider.css";
 
 const images = [
-  "/images/slider1.png",
+  "/images/slider1.jpg",
   "/images/slider2.png",
   "/images/slider3.png",
-  "/images/slider4.png",
+  "/images/slider4.jpg",
   "/images/slider5.png",
-  "/images/slider6.png"
+  "/images/slider6.png",
+  "/images/slider7.png",
+  "/images/slider8.png",
+  "/images/slider9.jpg",
+  "/images/slider10.jpg",
 ];
+
+// Split images into top and bottom rows
+const topImages = images.slice(0, 5);
+const bottomImages = images.slice(5);
 
 const ImageSlider = () => {
   return (
@@ -22,9 +30,9 @@ const ImageSlider = () => {
         animate={{ x: ["0%", "-100%"] }}
         transition={{ repeat: Infinity, duration: 20, ease: "linear" }}
       >
-        {[...images, ...images].map((img, index) => (
+        {[...topImages, ...topImages].map((img, index) => (
           <div className="slider-image" key={`top-${index}`}>
-            <img src={img} alt={`slide-${index}`} />
+            <img src={img} alt={`top-slide-${index}`} />
           </div>
         ))}
       </motion.div>
@@ -35,9 +43,9 @@ const ImageSlider = () => {
         animate={{ x: ["-100%", "0%"] }}
         transition={{ repeat: Infinity, duration: 20, ease: "linear" }}
       >
-        {[...images, ...images].map((img, index) => (
+        {[...bottomImages, ...bottomImages].map((img, index) => (
           <div className="slider-image" key={`bottom-${index}`}>
-            <img src={img} alt={`slide-${index}`} />
+            <img src={img} alt={`bottom-slide-${index}`} />
           </div>
         ))}
       </motion.div>

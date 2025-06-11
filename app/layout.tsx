749a13d146e204d6dbcb098a9b2ddb5197aebc18
muhammadsaad123-coder <130562696+muhammadsaad123-coder@users.noreply.github.com>
2025-06-12 00:16:35 +0500

    chnages

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,16 @@ import Navbar from "@/components/Navbar/Navbar";
 import Footer from "@/components/Footer/Footer";
 import CursorFollower from "@/components/CursorFollower/CursorFollower"
 // import SmoothScroll from "@/components/SmoothScroll/SmoothScroll";
+import { DM_Sans } from "next/font/google";
+
+const dmSans = DM_Sans({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"], // Customize weights as needed
+  variable: "--font-dm-sans",    // Use as a CSS variable
+  display: "swap",
+});
+
+
 
 export const metadata = {
   title: "Marq Network",
@@ -21,7 +31,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html  lang="en" className={dmSans.variable}>
       <body >
               {/* <SmoothScroll> */}
          <CursorFollower />

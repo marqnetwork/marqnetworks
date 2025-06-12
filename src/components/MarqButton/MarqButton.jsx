deeb5df5715b59f2afb0252323711cb29a90fabd
muhammadsaad123-coder <130562696+muhammadsaad123-coder@users.noreply.github.com>
2025-06-12 22:51:01 +0500

    bold

--- src/components/MarqButton/MarqButton.jsx
+++ src/components/MarqButton/MarqButton.jsx
@@ -2,13 +2,17 @@
 import React from "react";
 
 const MarqButton = ({ className = "", style = {} }) => {
+  const defaultStyle = {
+    fontWeight: 500, 
+    ...style,   
+    };    
   return (
     <a
       href="https://marqnetworks.zohobookings.com/#/business-consultation"
       target="_blank"
       rel="noopener noreferrer"
     >
-      <button className={className} style={style}>
+      <button className={className} style={defaultStyle}>
         Meet MarQ Networks
       </button>
     </a>

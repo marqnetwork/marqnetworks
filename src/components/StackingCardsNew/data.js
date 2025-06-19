64cf4d74641b6b04db49000c28f6c96e9ade4e19
muhammadsaad123-coder <130562696+muhammadsaad123-coder@users.noreply.github.com>
2025-06-20 01:04:14 +0500

    update

--- src/components/StackingCardsNew/data.js
+++ src/components/StackingCardsNew/data.js
@@ -1,24 +1,9 @@
+// data.js
 export const projects = [
-  {
-    // title: "Matthias Leidinger",
-    // description: "Originally hailing from Austria, Berlin-based photographer Matthias Leindinger is a young creative brimming with talent and ideas.",
-    src: "/images/port1.png",
-    // link: "https://images.unsplash.com/photo-1560472354-b33ff0c44a43?w=800&h=600&fit=crop",
-    // color: "#BBACAF"
-  },
-  {
-    // title: "Clément Chapillon",
-    // description: "This is a story on the border between reality and imaginary, about the contradictory feelings that the insularity of a rocky, arid, and wild territory provokes”—so French photographer Clément Chapillon describes his latest highly captivating project Les rochers fauves (French for ‘The tawny rocks’).",
-    src: "/images/port2.png",
-    // link: "https://www.ignant.com/2022/09/30/clement-chapillon-questions-geographical-and-mental-isolation-with-les-rochers-fauves/",
-    // color: "#977F6D"
-  },
-  {
-    // title: "Zissou",
-    // description: "Though he views photography as a medium for storytelling, Zissou’s images don’t insist on a narrative. Both crisp and ethereal, they’re encoded with an ambiguity—a certain tension—that lets the viewer find their own story within them.",
-    src: "/images/port3.png",
-    // link: "https://www.ignant.com/2023/10/28/capturing-balis-many-faces-zissou-documents-the-sacred-and-the-mundane-of-a-fragile-island/",
-    // color: "#C2491D"
-  },
-
-]
+  { src: "/images/port1.png" },
+  { src: "/images/port2.png" },
+  { src: "/images/port3.png" },
+  { src: "/images/port4.png" },
+  { src: "/images/port5.png" },
+  { src: "/images/port6.png" }
+];

9bdb8fd498abed16ba278bc56bbefaf74a1cbf99
muhammadsaad123-coder <130562696+muhammadsaad123-coder@users.noreply.github.com>
2025-06-25 01:47:10 +0500

    simple

--- app/page.tsx
+++ app/page.tsx
@@ -1,49 +1,21 @@
 "use client";
-
-import React from "react";
-import Head from "next/head";
-import dynamic from "next/dynamic";
-
-// Immediate load component
 import Hero from "@/components/Hero/Hero";
+import AboutSection from "@/components/AboutSection/AboutSection";
+import ResultsSection from "@/components/ResultsSection/ResultsSection";
+import Work from "@/components/Work/Work";
+import FeatureSection from "@/components/FeatureSection/FeatureSection";
+import FeatureGridSection from "@/components/FeatureGridSection/FeatureGridSection";
+import LandinBenefits from "@/components/LandinBenefits/LandinBenefits";
+import LandinBenefitCards from "@/components/LandinBenefitCards/LandinBenefitCards";
+import OurServices from "@/components/OurServices/OurServices";
+import PortfolioSection from "@/components/PortfolioSection/PortfolioSection";
+import LaunchSection from "@/components/LaunchSection/LaunchSection";
+import FAQSection from "@/components/FAQSection/FAQSection";
+import JoinUsNow from "@/components/JoinUsNow/JoinUsNow";
 
-// Lazy-loaded components
-const AboutSection = dynamic(
-  () => import("@/components/AboutSection/AboutSection")
-);
-const ResultsSection = dynamic(
-  () => import("@/components/ResultsSection/ResultsSection")
-);
-const Work = dynamic(() => import("@/components/Work/Work"));
-const FeatureSection = dynamic(
-  () => import("@/components/FeatureSection/FeatureSection")
-);
-const FeatureGridSection = dynamic(
-  () => import("@/components/FeatureGridSection/FeatureGridSection")
-);
-const LandinBenefits = dynamic(
-  () => import("@/components/LandinBenefits/LandinBenefits")
-);
-const LandinBenefitCards = dynamic(
-  () => import("@/components/LandinBenefitCards/LandinBenefitCards")
-);
-const PortfolioSection = dynamic(
-  () => import("@/components/PortfolioSection/PortfolioSection")
-);
-const OurServices = dynamic(
-  () => import("@/components/OurServices/OurServices")
-);
-const LaunchSection = dynamic(
-  () => import("@/components/LaunchSection/LaunchSection")
-);
-const FAQSection = dynamic(() => import("@/components/FAQSection/FAQSection"));
-const JoinUsNow = dynamic(() => import("@/components/JoinUsNow/JoinUsNow"));
 
 const Home = () => {
-  // const title =
-  //   "MarQ Networks | AI-Driven Digital Growth Partner | SEO, CRM & Web Solutions";
-  // const description =
-  //   "Unlock scalable growth with MarQ Networks, your AI-driven partner for digital transformation. Specializing in advanced SEO audits, smart CRM automation, and custom website development—accelerate your business results today.";
+  
 
   return (
     <>

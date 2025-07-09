ce2f3b16bede833f734265e119c2d8860d0105aa
Saif Ali <90478821+isaifalikhan@users.noreply.github.com>
2025-07-09 22:15:16 +0500

    fixes

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,28 +1,12 @@
-export async function generateStaticParams() {
-  const res = await fetch('http://localhost/wordpress/wp-json/wp/v2/posts');
-  const posts = await res.json();
+import BlogPost from '@/components/Slug/Slug';
+import React from 'react';
 
-  return posts.map((post: any) => ({
-    slug: post.slug,
-  }));
-}
-
-export async function fetchPost(slug: string) {
-  const res = await fetch(`http://localhost/wordpress/wp-json/wp/v2/posts?slug=${slug}&_embed`);
-  const post = await res.json();
-  return post[0];
-}
-
-export default async function BlogPost({ params }: { params: { slug: string } }) {
-  const post = await fetchPost(params.slug);
-
+const SlugPage = ({ params }: { params: { slug: string } }) => {
   return (
-    <article style={{ padding: '2rem', color: 'white', backgroundColor: 'black' }}>
-      <h1
-        dangerouslySetInnerHTML={{ __html: post.title.rendered }}
-        style={{ color: 'white' }}
-      />
-      <div dangerouslySetInnerHTML={{ __html: post.content.rendered }} />
-    </article>
+    <div>
+      <BlogPost params={{ slug: params.slug }} />
+    </div>
   );
-}
+};
+
+export default SlugPage;

c7f2ef00493d1e54527bb041675ce91f6a3d09be
Saif Ali <90478821+isaifalikhan@users.noreply.github.com>
2025-07-11 00:31:44 +0500

    ff

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,30 +1,40 @@
-// app/blog/[slug]/page.tsx
-import { fetchPost } from '../../lib/fetchPost';
+// // app/blog/[slug]/page.tsx
+// import { fetchPost } from '../../lib/fetchPost';
 
-export async function generateStaticParams() {
-  const res = await fetch('https://wordpress.org/news/wp-json/wp/v2/posts');
-  const posts = await res.json();
+// export async function generateStaticParams() {
+//   const res = await fetch('https://wordpress.org/news/wp-json/wp/v2/posts');
+//   const posts = await res.json();
 
-  return posts.map((post: any) => ({
-    slug: post.slug,
-  }));
-}
+//   return posts.map((post: any) => ({
+//     slug: post.slug,
+//   }));
+// }
 
 
-export default async function Page({
-  params,
-}: {
-  params: { slug: string };
-}) {
-  const post = await fetchPost(params.slug);
+// export default async function Page({
+//   params,
+// }: {
+//   params: { slug: string };
+// }) {
+//   const post = await fetchPost(params.slug);
 
+//   return (
+//     <article style={{ padding: "2rem", color: "white", backgroundColor: "black" }}>
+//       <h1
+//         dangerouslySetInnerHTML={{ __html: post.title.rendered }}
+//         style={{ color: "white" }}
+//       />
+//       <div dangerouslySetInnerHTML={{ __html: post.content.rendered }} />
+//     </article>
+//   );
+// }
+
+import React from 'react'
+
+const page = () => {
   return (
-    <article style={{ padding: "2rem", color: "white", backgroundColor: "black" }}>
-      <h1
-        dangerouslySetInnerHTML={{ __html: post.title.rendered }}
-        style={{ color: "white" }}
-      />
-      <div dangerouslySetInnerHTML={{ __html: post.content.rendered }} />
-    </article>
-  );
+    <div>page</div>
+  )
 }
+
+export default page

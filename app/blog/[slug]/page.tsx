f00d38cb57328265bcaa05927659c665e068beb8
Saif Ali <90478821+isaifalikhan@users.noreply.github.com>
2025-07-11 00:21:04 +0500

    fixes

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -2,7 +2,7 @@
 import { fetchPost } from '../../lib/fetchPost';
 
 export async function generateStaticParams() {
-  const res = await fetch('http://localhost/wordpress/wp-json/wp/v2/posts');
+  const res = await fetch('https://demo.wp-api.org/wp-json/wp/v2/posts');
   const posts = await res.json();
 
   return posts.map((post: any) => ({

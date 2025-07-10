d1ac56f126e67a3061ee8606ece8f0836a912025
muhammadsaad123-coder <130562696+muhammadsaad123-coder@users.noreply.github.com>
2025-07-10 23:20:21 +0500

    changes

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -28,7 +28,7 @@ const Hero = () => {
       <div className="hero__container">
         <div className="hero__content">
           <h2 className={`hero-heading ${animateHead ? "animate-heading" : ""}`}>
-            <span className="heading-line">Premium Digital Partner  </span>
+            <span className="heading-line">Premium Digital Partner Saad </span>
             <span className="heading-line">for Visionary Brands</span>
           </h2>
 

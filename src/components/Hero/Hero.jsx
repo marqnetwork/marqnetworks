6adc6ba19f37997c0c13070493610ee320fe357e
muhammadsaad123-coder <130562696+muhammadsaad123-coder@users.noreply.github.com>
2025-07-15 18:23:47 +0500

    fix

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -14,7 +14,10 @@ const Hero = () => {
           </h2>
 
           <p className="hero__para">
-            “Strategy, design, code, and growth—engineered as one seamless stack. From AI-driven digital transformation consultancy and branding to custom software development and funnel marketing with CRM automation, MarQ Networks turns clicks into revenue for scaling teams across the U.S. & GCC. Think of us as your all-in-one branding agency for startups, custom dev shop, and funnel marketing agency—powered by AI. Our 5-Pillar Service Stack (Strategy & Digital Transformation • Brand + Experience Design • Custom Software & Automation • Growth Marketing & Lead Gen • Off-Shore Excellence Hub) delivers launch-ready products in ≤45 days, trims costs up to 40 %, and compounds growth with AI-powered CRM workflow automation. Let’s build smarter, faster, together.”
+            “Strategy, design, code, and growth—engineered as one seamless stack. From AI-driven digital transformation consultancy and branding to custom software development and funnel marketing with CRM automation, MarQ Networks turns clicks into revenue for scaling teams across the U.S. & GCC.
+            <br/>
+            <br />
+             Think of us as your all-in-one branding agency for startups, custom dev shop, and funnel marketing agency—powered by AI. Our 5-Pillar Service Stack (Strategy & Digital Transformation Brand + Experience Design Custom Software & Automation Growth Marketing & Lead Gen Off-Shore Excellence Hub) delivers launch-ready products in ≤45 days, trims costs up to 40 %, and compounds growth with AI-powered CRM workflow automation. Let’s build smarter, faster, together.”
           </p>
 
           <div className="hero__buttons">

8b4e18e7aeb0c851749458fd98ed3e8eef108c78
muhammadsaad123-coder <130562696+muhammadsaad123-coder@users.noreply.github.com>
2025-07-29 01:48:35 +0500

    change

--- app/Blogs/page.tsx
+++ app/Blogs/page.tsx
@@ -39,10 +39,10 @@ export default function StoryListPage() {
     <main className="bg-black text-white min-h-screen flex flex-col items-center px-4 py-16">
       {/* Hero Section */}
       <section className="w-full max-w-[1200px] text-center mb-20 h-[80vh] flex flex-col justify-center items-center gap-3">
-        <h1 className="text-6xl md:text-5xl font-bold leading-tight mb-6 fontt">
-          Real-World Growth Insights <br /> Powered by AI
+        <h1 className="text-6xl md:text-5xl font-bold leading-tight mb-4 fontt  widthh">
+          Real-World Growth Insights <br /> Powered by AI 
         </h1>
-        <p className="text-lg text-gray-400 mb-8 w-full max-w-[90%] sm:max-w-[500px] md:max-w-[650px] text-center md:text-2xl">
+        <p className="text-lg text-gray-400 mb-8 w-full max-w-[100%] sm:max-w-[500px] md:max-w-[1000px] text-center md:text-2xl">
           Get strategic advice on AI automation, funnel marketing, conversion-focused branding, and scalable software development from experts who've actually delivered growth.
         </p>
         <div className="flex justify-center gap-4">
@@ -56,7 +56,7 @@ export default function StoryListPage() {
       </section>
 
       {/* Filter Tabs */}
-      <div className="w-full max-w-[1200px] mb-8">
+      <div className="w-full max-w-[1200px] mb-3">
         <div className="flex justify-center gap-4 flex-wrap">
           {filterOptions.map((option) => (
             <button
